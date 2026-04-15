b5be629d22d64c6baff18468c9a5020c8ada3d92
lorna88 <lorna_88@mail.ru>
2026-04-15 18:34:26 +0300

    Удален mock для магазинов (скучный)

--- business_mock/urls.py
+++ business_mock/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from business_mock.views_orders import OrderMockView
 from business_mock.views_products import ProductMockView
-from business_mock.views_stores import StoreMockView
 
 app_name = 'mock'
 
@@ -11,6 +10,4 @@ urlpatterns = [
     path('orders/<int:pk>/', OrderMockView.as_view(), name='mock_order_detail'),
     path('products/', ProductMockView.as_view(), name='mock_products'),
     path('products/<int:pk>/', ProductMockView.as_view(), name='mock_product_detail'),
-    path('stores/', StoreMockView.as_view(), name='mock_stores'),
-    path('stores/<int:pk>/', StoreMockView.as_view(), name='mock_store_detail'),
 ]
